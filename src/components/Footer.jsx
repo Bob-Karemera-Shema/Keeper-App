b31cc9137031887a37ddb97cf21e65f01226fdfb
Bob-Karemera-Shema <72112797+Bob-Karemera-Shema@users.noreply.github.com>
2022-11-04 13:11:41 +0000

    Retrieving notes from notes json + using map.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer(){
     return (<footer>
-        <p>Copyright {new Date().getFullYear()}</p>
+        <p>Copyright © {new Date().getFullYear()}</p>
     </footer>);
 }
 
